99834e1349eb6fb4303137de11a007e10c401228
John Gomez <jgomez818@gmail.com>
2015-06-29 00:38:13 -0400

    Adding RecyclerView to weather and apod layouts. Needs adapter

--- app/src/main/java/lighterletter/c4q/nyc/skyleap/ApodActivity.java
+++ app/src/main/java/lighterletter/c4q/nyc/skyleap/ApodActivity.java
@@ -9,6 +9,8 @@ package lighterletter.c4q.nyc.skyleap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +19,21 @@ import android.view.ViewGroup;
  * Created by hp1 on 21-01-2015.
  */
 public class ApodActivity extends Fragment {
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View v = inflater.inflate(R.layout.activity_apod,container,false);
+//        return v;
+//    }
+
+    public RecyclerView recyclerView;
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_apod,container,false);
-        return v;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_apod, container, false);
+        recyclerView = (RecyclerView)rootView.findViewById(R.id.WeatherListRV);
+        getLayoutInflater(savedInstanceState).inflate(R.layout.activity_apod, container, false);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        return rootView;
     }
 }
 
--- app/src/main/java/lighterletter/c4q/nyc/skyleap/WeatherActivity.java
+++ app/src/main/java/lighterletter/c4q/nyc/skyleap/WeatherActivity.java
@@ -10,21 +10,44 @@ package lighterletter.c4q.nyc.skyleap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by hp1 on 21-01-2015.
- */
+
+
 public class WeatherActivity extends Fragment {
 
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View v =inflater.inflate(R.layout.activity_weather,container,false);
+//        return v;
+//
+//    }
+
+    public RecyclerView recyclerView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.activity_weather,container,false);
-        return v;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_weather, container, false);
+        recyclerView = (RecyclerView)rootView.findViewById(R.id.WeatherListRV);
+        getLayoutInflater(savedInstanceState).inflate(R.layout.activity_weather, container, false);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return rootView;
     }
 }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.fragment_catalog_viewer, container, false);
+//        recyclerView = (RecyclerView)rootView.findViewById(R.id.my_recycler_view);
+//        getLayoutInflater(savedInstanceState).inflate(R.layout.catalog_child, container);
+//        return rootView;
+//    }
 
 
 //public class WeatherActivity extends ActionBarActivity {
